af8a63a488f83295dd791f31386a0a1757764644
Hibikine Kage <goodsmile314159@gmail.com>
2018-02-22 13:21:56 +0900

    feat: 画像書き出せるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate image;
+use image::ImageBuffer;
+use image::Rgb;
+use std::fs::File;
+use image::ImageRgb8;
+use image::PNG;
+
 fn main() {
-    println!("Hello, world!");
+    let nx = 200;
+    let ny = 100;
+    let mut image = ImageBuffer::new(nx, ny);
+
+for (x, y, pixel) in image.enumerate_pixels_mut() {    
+        let r = (std::u8::MAX as u32 * x / nx) as u8;
+        let g = (std::u8::MAX as u32 * y / ny) as u8;
+        let b = std::u8::MAX as u8 >> 1;
+        *pixel = Rgb([r, g, b]);
+    }
+    
+    let ref mut f = File::create("image.png").unwrap();
+    ImageRgb8(image).save(f, PNG).unwrap();
 }
